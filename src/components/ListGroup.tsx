73488e770e46ca9483bff6ee42deac4e5b781223
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-26 11:59:28 +0200

    first commit of the day

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -10,10 +10,10 @@ function ListGroup({items, heading}: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
-    <>
+    <div className="m-5">
       <h1>{heading}</h1>
       {items.length === 0 && <p>There are no items!</p>}
-      <ul className="list-group">
+      <ul className="list-group col-12 col-lg-3 col-md-6">
         {items.map((item, index) => (
           <li
             key={item}
@@ -24,7 +24,7 @@ function ListGroup({items, heading}: ListGroupProps) {
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
